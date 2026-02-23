f73bc280fa37a32591c330f125678b51fe55600e
mhekhay1 <nakasky255@gmail.com>
2026-02-22 21:06:51 -0500

    Add files via upload

---
+++ participation.py
@@ -0,0 +1,14 @@
+age = 41
+
+if age < 2:
+    print("The person is a baby.")
+elif age < 4:
+    print("The person is a toddler.")
+elif age < 13:
+    print("The person is a child.")
+elif age < 20:
+    print("The person is a teenager.")
+elif age < 60:
+    print("The person is an adult.")
+else:
+    print("The person is an elder.")
